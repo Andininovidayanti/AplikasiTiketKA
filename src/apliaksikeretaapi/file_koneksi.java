cea6508230d95eb21a102f4e2ec90974d108ce26
Andinovidayanti <andininovi09@gmail.com>
2017-03-17 23:09:12 +0700

    Initial commit

---
+++ src/apliaksikeretaapi/file_koneksi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apliaksikeretaapi;
+
+/**
+ *
+ * @author Andini Novidayanti A
+ */
+public class file_koneksi {
+
+    static Object GetConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
